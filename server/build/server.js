1df8d9ee9b87ddeaba591b0fc8e326b88b94090d
Matheus Mendes <matheush.1998@hotmail.com>
2022-09-14 15:05:16 -0300

    Create Vite

---
+++ server/build/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+//ECMA Script modules
+const app = express();
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, name: 'Anuncio' },
+        { id: 2, name: 'Anuncio2' },
+        { id: 3, name: 'Anuncio3' }
+    ]);
+});
+app.listen(3333);
