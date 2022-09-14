bfb98d93822ddaf25ed56ad59b7e7c0661372fd6
Matheus Mendes <matheush.1998@hotmail.com>
2022-09-14 20:37:51 -0300

    Assets folder with tailwinds

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -1,5 +1,11 @@
+import './assets/styles/main.css';
+
 function App() {
-  return (<h1>Hello  World</h1>)
+  return (
+    <div className="w-8 h-8 bg-violet-500">  
+    </div>
+  )
 }
 
 export default App
+//jsxDEV as _jsxDEV from  react/jsx-dev-runtime
