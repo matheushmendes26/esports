a485151d4c4becf72cf39e4a74820dba62fc1590
Matheus Mendes <matheush.1998@hotmail.com>
2022-09-15 13:04:43 -0300

    mobile frontend

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,35 @@
 import {View} from 'react-native';
 
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
+
+import {StatusBar} from 'react-native';
+
 import {Background} from './src/components/Background'
+import { Loading } from './src/components/Loading/Loadingindex';
+import {Home} from './src/components/screens/Home';
 
 export default function App() {
+ const [fontsLoaded] =  useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     <Background>
+     <StatusBar 
+    barStyle="light-content" backgroundColor="transparent" 
+    translucent/>
+    {fontsLoaded ? <Home /> : <Loading /> }
     </Background>
   );
 }
