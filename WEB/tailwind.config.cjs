7646dbc88041a808e17be9396f1d247e5b7c0b9b
Matheus Mendes <matheush.1998@hotmail.com>
2022-09-15 07:35:27 -0300

    Frontend Web-Page/ first incrementations

--- WEB/tailwind.config.cjs
+++ WEB/tailwind.config.cjs
@@ -2,9 +2,15 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        galaxy: "url('/background__galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 33.08%, #43E7AD 33.94%, #E1D55D 34.57%)', 
+      }
+    },
   },
   plugins: [],
 }
