69ff28f759550ebbb5209dff6db60abdbd361d99
Matheus Mendes <matheush.1998@hotmail.com>
2022-09-15 21:39:54 -0300

    Prisma and database on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,32 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response)=>{
+//HTTP METHODS / API RESTFUL
+
+//GET, POST, PUT, PATCH, DELETE
+
+app.get('/games', (request, response)=>{
+  return response.json([
+
+  ]);
+} );
+
+app.post('/ads', (request,  response)=>{
+  return response.status(201).json([
+    
+  ]);
+} );
+
+app.get('games/:id/ads', (request, response)=>{
   return response.json([
     {id: 1, name: 'Anuncio'},
     {id: 2, name: 'Anuncio2'},
     {id: 3, name: 'Anuncio3'}
   ])
+})
+
+app.get('ads/:id/discord', (request, response)=>{
+  return response.json([])
 })
 
 app.listen(3333)
